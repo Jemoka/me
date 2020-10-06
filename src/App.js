f8e244806aa9386ea0ffd8bfe10becb964d2acfa
Houjun Liu <hliu@shabang.cf>
2020-10-05 22:47:24 -0700

    nice!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faRss, faLink } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faMedium } from '@fortawesome/free-brands-svg-icons'
+
+
+
 function App() {
+    let greetings = ["Yo", "What's up", "Heyo", "Hey"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="landing">
+        <div className="centre-hero">
+            <div className="impact">{greetings[Math.floor(Math.random() *greetings.length)]}, I am <div style={{fontWeight: 600, display: "inline"}}>Houjun Liu.</div></div>
+                <div className="subtitle">Student, Software Engineer, Interface Designer, Content Creator</div>
+                <div className="contact-tray">
+                    <FontAwesomeIcon className="contact-icon contact-twitter" icon={faTwitter} />
+                    <FontAwesomeIcon  className="contact-icon contact-medium" icon={faMedium} />
+                    <FontAwesomeIcon  className="contact-icon contact-email" icon={faEnvelope} />
+                    <FontAwesomeIcon  className="contact-icon contact-pod" icon={faRss} />
+                </div>
+        </div>
+            <div style={{padding: 10, bottom: 5, left:5, position: "fixed"}}>Under Construction. Obviously. <span style={{fontWeight: 400}}> 2020-2021 Houjun Liu.</span></div>
+            </div>
     </div>
   );
 }
