6160607e8da4bb959b25289776607f13de29467c
Houjun Liu <hliu@shabang.cf>
2021-06-01 10:00:08 -0700

    interesting!

--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,11 @@ export default function Home() {
             <hr />
             <br />
             <h1 className="text-xl font-bold">Who's this guy?</h1>
-            I am a <a href="https://avatars.githubusercontent.com/u/28765741?v=4">human</a> interested in <a href="https://arxiv.org/abs/2104.10661">linguistic analysis</a>, <a href="https://www.preprints.org/manuscript/202004.0214/v1">social metrics</a>, and <a href="https://www.shabang.cf/">user interfaces</a>. <a href="https://en.wikipedia.org/wiki/Artificial_general_intelligence">AGI</a> & <a href="https://www.vim.org/">Vim</a> are cool. I run <a href="https://www.condution.com/">Condution</a> and <a href="https://www.shabang.cf/">Shabang</a>, do <a href="https://arxiv.org/abs/2104.10661">research on NLP</a>, and recently took over ops at <a href="http://modap.io/">MODAP</a>. Oh right, I also go to school at <a href="https://www.nuevaschool.org/">The Nueva School.</a>
+            I am a <a href="https://avatars.githubusercontent.com/u/28765741?v=4">human</a> interested in <a href="https://arxiv.org/abs/2104.10661">linguistic analysis</a>, <a href="https://www.preprints.org/manuscript/202004.0214/v1">social metrics</a>, and <a href="https://www.shabang.cf/">user interfaces</a>. <a href="https://en.wikipedia.org/wiki/Artificial_general_intelligence">AGI</a> & <a href="https://www.vim.org/">Vim</a> are cool. I run <a href="https://www.condution.com/">Condution</a> and <a href="https://www.shabang.cf/">Shabang</a>, do <a href="https://arxiv.org/abs/2104.10661">research on NLP</a>, and recently took over ops at <a href="http://modap.io/">MODAP</a>. Oh right, I also go to school at <a href="https://www.nuevaschool.org/">The Nueva School.</a> 
             <br />
             Need to catch me? <a href="mailto:hliu@shabang.cf">hliu@shabang.cf</a>. Please do email me, I actually check.
+            <br />
+            Here are a few things that <a href="https://interesting.jemoka.com/">I find interesting</a>, in case you are wondering.
             <br />
             <br />
             <h1 className="text-xl font-bold">Recent Projects</h1>
